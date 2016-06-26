f5fc89c2b194dfb90994a95af77bbbc91d2a6a85
BrandonSpitler <brandonfspitler@gmail.com>
2016-06-26 00:21:35 -0600

    manager features
    added manager feature and queries the database based on email

--- addingTaskPopUp.php
+++ addingTaskPopUp.php
@@ -5,7 +5,7 @@ Enter file contents here<!DOCTYPE html>
 <form action="" method="post">
 	Task Id#<br>
 	<input type "text" name="u_id"><br>
-	worker 1<br>
+	workers email<br>
 		<input type "text" name="u_worker1"><br>
 	Task Description<br>
 		<input type "text" name="u_TaskDescription"><br>
@@ -19,26 +19,36 @@ Enter file contents here<!DOCTYPE html>
 <?php
 include("includes/connection.php");
 if(isset($_POST['b_Submit_Task'])){
-	$taskId=$_POST['u_id'];
-	$difficulty=$_POST['u_difficulty'];
-	$worker1=$_POST['u_worker1'];
-	$manager="testerManager";
-	$description=$_POST['u_TaskDescription'];
-	$due = $_POST['u_dateDue'];
-	$assigned = "2016-08-12";
-	$progress = "Not started";
-	$insert="insert into `InProgress` (`id`, `manager`, `workers`, `description`, `difficulty`, `due`, `assigned`, `progress`) values ('$taskId','your mother','$worker1','$description','$difficulty','$due',now(),'Not stated')";
+	$worker1_email=$_POST['u_worker1'];
+	$name="SELECT name, manager FROM users WHERE username='$worker1_email'";
+	$run_email = mysqli_query($con,$name);
+	$row=mysqli_fetch_array($run_email);
+	$name=$row['name'];
+	$check = mysqli_num_rows($run_email);
+	if($check==1){
+		
+		$taskId=$_POST['u_id'];
+		$difficulty=$_POST['u_difficulty'];
+		$manager=$row['manager'];
+		$description=$_POST['u_TaskDescription'];
+		$due = $_POST['u_dateDue'];
+		$assigned = "2016-08-12";
+		$progress = "Not started";
+		$insert="insert into `InProgress` (`id`, `manager`, `workers`, `description`, `difficulty`, `due`, `assigned`, `progress`) values ('$taskId','$manager','$name','$description','$difficulty','$due',now(),'Not stated')";
 
-	$run_insert = mysqli_query($con,$insert);
-	if (false==$run_insert){
-		printf("error: %s\n", mysqli_error($con));
-	}		
-	if($run_insert){
+		$run_insert = mysqli_query($con,$insert);
+		if (false==$run_insert){
+			printf("error: %s\n", mysqli_error($con));
+		}		
+		if($run_insert){
 
 	
-        	echo "<script>window.open('home.php','_self')</script>";
+        		echo "<script>window.open('home.php','_self')</script>";
+		}
+	}else{
+		printf("error users email is not in the system please insert a email of a worker signed up for taskHub");
 	}
-        }
+}
 
 ?>
 
