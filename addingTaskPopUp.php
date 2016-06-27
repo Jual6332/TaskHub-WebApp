e1c614c88c0020b89b44e5ea578a6d241c8c9c3a
BrandonSpitler <brandonfspitler@gmail.com>
2016-06-26 21:36:27 -0600

    Update addingTaskPopUp.php
    Made it so that there can be more then one manager

--- addingTaskPopUp.php
+++ addingTaskPopUp.php
@@ -21,42 +21,30 @@
 include("includes/connection.php");
 session_start();
 $user=$_SESSION['user_email'];
-$get_userID = mysqli_query($con,"select * from users where username='$user'");
+$get_userID = mysqli_query($con,"select * from users where username ='$user'");
 $get_userID = mysqli_fetch_array($get_userID);
-$user_emps = $get_userID['employees'];
-$emp_array = explode(',',$user_emps);
+$id = $get_userID['id'];
+
+$employees=explode(",",$get_userID['employees']);
 
 echo "your current employees names and emails are";
-echo "<div>";
-echo "<table border='1' style='width:25%'>";
-echo "<tr>";
-echo "<td><b>Employees</b></td>";
-echo "<td><b>Emails</b></td>";
-echo "</tr>";
-for($j=0;$j < count($emp_array);$j++){
-	$cur_emp_id = (int)$emp_array[$j];
-	$getEmployees = mysqli_query($con,"SELECT * FROM `users` WHERE `id` = '$cur_emp_id'");
-	$row6 = mysqli_fetch_array($getEmployees);
-	$user_full_name = $row6['name'];
-	$user_email = $row6['username'];
-	echo "<tr><td>$user_full_name</td>";
-	echo "<td>$user_email</td></tr>";
-}
-echo "</table>";
-echo "</div>";
-/*echo "<table border=1>
+echo "<table border=1>
 	<tr>
 
 	<th> Employees Name</th>
 	<th> Email </th>
 	</tr>";
-while($record = mysqli_fetch_array($getEmployees)){
+foreach($employees as $employId){
+
+	$getEmployID=mysqli_query($con,"select *from users where id='$employId'");
+	//$getEmployID=mysqli_fetch_array($getEmployID);
+while($record = mysqli_fetch_array($getEmployID)){
 	echo "<tr>";
 	echo "<td>" . $record['name'] . "</td>";
 	echo "<td>" . $record['username'] . "</td>";
 	echo "</tr>";
-}
-echo "</table>";*/
+}}
+echo "</table>";
 
 if(isset($_POST['b_Submit_Task'])){	
 	
@@ -69,6 +57,8 @@ if(isset($_POST['b_Submit_Task'])){
 	$name=$row['name'];
 	$check = mysqli_num_rows($run_email);
 	if($check==1){
+	
+		
 		if($id==$row['manager']){
 		
 			$difficulty=$_POST['u_difficulty'];
