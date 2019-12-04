299a46a3bdd75be74da39882018b9bac1bb17443
Justin Alvey <jual6332@colorado.edu>
2019-12-04 16:11:44 -0500

    Updated header.php
    Removed unneccessary comments and white space.

--- template/header.php
+++ template/header.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,19 +21,9 @@
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-    <!--<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>-->
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
-
 <body>
-
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
         <div class="container topnav">
@@ -48,25 +37,9 @@
                 </button>
                 <a class="navbar-brand topnav" href="index.html">TaskHub</a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="#about">About the Team</a>
-                    </li>
-                    <li>
-                        <a href="index.html">Services</a>
-                    </li>
-                    <li>
-                        <a href="sign_up.html">Sign Up/Login</a>
-                    </li>
-                </ul>
-            </div>-->
-            <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
     </nav>
-
 
     <!-- Header -->
     <a name="about"></a>
@@ -159,23 +132,8 @@
                 </form>-->
                                 
                                 </div>
-                                
                               </div><!-- tab-content -->
-                              
                         </div> <!-- /form -->
-
-                        <!--
-                        <ul class="list-inline intro-social-buttons">
-                            <li>
-                                <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
-                            </li>
-                        </ul>-->
                     </div>
                 </div>
             </div>
@@ -185,8 +143,7 @@
 
     </div>
     <!-- /.intro-header -->
-
-
+    
     <!-- Footer -->
     <footer>
         <div class="container">
@@ -202,8 +159,6 @@
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="index.html">Services</a>
-                        </li>
                         <li class="footer-menu-divider">&sdot;</li>
                         <li>
                             <a href="sign_up.html">Sign Up/Login</a>
@@ -216,7 +171,7 @@
                         <li>This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</li>
                     </ul>-->
                     </p>
-                    <p class="copyright text-muted small">Copyright &copy; TaskHub 2016. All Rights Reserved</p>
+                    <p class="copyright text-muted small">Copyright &copy; TaskHub 2019. All Rights Reserved</p>
                 </div>
             </div>
         </div>
@@ -228,9 +183,7 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
     <script src="js/index.js"></script>
 
 </body>
-
 </html>
