5b471a97d7950ccaaa82b69a892babee24bcdabb
Justin Alvey <jual6332@colorado.edu>
2023-05-10 23:15:44 -0400

    Wrote in new task data for employees

--- addDataToInProgress.sql
+++ addDataToInProgress.sql
@@ -7,3 +7,7 @@ INSERT INTO `InProgress` (`id`, `manager`, `workers`, `description`, `difficulty
 (6,'Sean Oneal', 'Justin Alvey', 'create USER-2 function', 3, '2016-06-23', '2016-06-11', 'Testing'),
 (7,'Sean Oneal', 'Justin Alvey', 'create USER-3 function', 2, '2016-06-22', '2016-06-12', 'Designing'),
 (8,'Sean Oneal', 'Brandon Spitler', 'create Func-01 fucntion', 2, '2016-06-17', '2016-06-11', 'Have not started');
+
+INSERT INTO `InProgress` (`id`, `manager`, `workers`, `description`, `difficulty`, `due`, `assigned`, `progress`) VALUES
+(9,'Justin Alvey', 'Sean Oneal', 'Create filterCircuitsNonProd function', 3, '2023-05-23', '2023-05-10', 'Just started'),
+(10,'Justin Alvey', 'Brandon Spitler', 'Update documentation on dashboard.php', 1, '2023-05-23', '2023-05-10', 'Just started');
