0e372a6b79f51bff9ce3e0126efdd616999608dc
Justin Alvey <jual6332@colorado.edu>
2023-05-10 21:43:30 -0400

    Wrote script to add data to InProgress table (sql)

---
+++ addDataToInProgress.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `InProgress`
+--
+
+INSERT INTO `InProgress` (`id`, `manager`, `workers`, `description`, `difficulty`, `due`, `assigned`, `progress`) VALUES
+(5,'Lucy Wilkinson', 'Sean Oneal', 'Create USER-1 function', 2, '2016-07-01', '2016-06-10', 'Just started'),
+(6,'Sean Oneal', 'Justin Alvey', 'create USER-2 function', 3, '2016-06-23', '2016-06-11', 'Testing'),
+(7,'Sean Oneal', 'Justin Alvey', 'create USER-3 function', 2, '2016-06-22', '2016-06-12', 'Designing'),
+(8,'Sean Oneal', 'Brandon Spitler', 'create Func-01 fucntion', 2, '2016-06-17', '2016-06-11', 'Have not started');
