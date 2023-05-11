0cb8bdf78ffc5cfc94aa9c492a65f21561f9b77d
Justin Alvey <jual6332@colorado.edu>
2023-05-10 21:43:06 -0400

    Wrote script to add data to CompletedTasks table (sql)

---
+++ addDataToCompletedTasks.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `CompletedTasks`
+--
+
+INSERT INTO `CompletedTasks` (`id`, `manager`, `workers`, `description`, `grade`, `completed`) VALUES
+(4, 'Lucy Wilkinson', 'Sean Oneal', 'Set up trello', 10, '2016-06-14'),
+(5, 'Lucy Wilkinson', 'Sean Oneal', 'Project part 1', 9, '2016-06-12'),
+(6, 'Sean Oneal', 'Justin Alvey', 'Team meating', 10, '2016-06-16'),
+(7, 'Sean Oneal', 'Brandon Spitler', 'create database', 8, '2016-06-16');
